90d60eb410549029ea990b478a35fe90dc086254
Diego H.L <diegoherreralopez9@gmail.com>
2025-06-10 10:22:14 -0600

    agrega prop opcional 'label' al componente Input
    Se extiende la interfaz InputProps para incluir una nueva propiedad opcional 'label',
    permitiendo mostrar una etiqueta asociada al campo de entrada.

--- src/componentes/ui/input.tsx
+++ src/componentes/ui/input.tsx
@@ -1,10 +1,20 @@
 import * as React from "react"
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, ...props }, ref) => {
-  return <input className={`input ${className || ""}`} ref={ref} {...props} />
-})
-Input.displayName = "Input"
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, label, ...props }, ref) => {
+    return (
+      <div className="flex flex-col gap-1">
+        {label && <label className="text-sm font-medium">{label}</label>}
+        <input className={`input ${className || ""}`} ref={ref} {...props} />
+      </div>
+    );
+  }
+);
 
-export { Input }
+Input.displayName = "Input";
+
+export { Input };
